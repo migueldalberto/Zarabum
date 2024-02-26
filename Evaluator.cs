5515c0e84dcf678e324b31c8326e844b422526b7
Miguel Dalberto <migueldalberto.acc@gmail.com>
2024-02-26 19:18:30 -0300

    add evaluation for parenthesis groupings

--- Evaluator.cs
+++ Evaluator.cs
@@ -6,6 +6,7 @@ static class Evaluator
     {
         Literal literal => literal.value == null ? 0.0 : (double)literal.value,
         Binary binary => EvaluateBinary(binary),
+        Grouping grouping => Evaluate(grouping.expression),
         _ => throw new Exception("unexpected expression")
     };
 
@@ -15,24 +16,14 @@ static class Evaluator
         var rightOperand = expr.rightOperand.Evaluate();
 
         if (expr.operatorToken.type == TokenType.PLUS)
-        {
             return leftOperand + rightOperand;
-        }
         else if (expr.operatorToken.type == TokenType.MINUS)
-        {
             return leftOperand - rightOperand;
-        }
         else if (expr.operatorToken.type == TokenType.STAR)
-        {
             return leftOperand * rightOperand;
-        }
         else if (expr.operatorToken.type == TokenType.SLASH)
-        {
             return leftOperand / rightOperand;
-        }
         else
-        {
             throw new Exception($"unexpected operator <{expr.operatorToken.type}>");
-        }
     }
 }
